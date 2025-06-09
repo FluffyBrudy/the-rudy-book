39873c164c78c7a4a51e654461b4bc092034af4e
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-06-09 14:29:38 +0545

    refactor: add validation constant `MAX_POST_TEXT_LENGTH` and `MAX_POST_TEXT_LENGTH`

--- src/constants/validation.ts
+++ src/constants/validation.ts
@@ -2,3 +2,6 @@ export const MIN_USERNAME_LENGTH = 3;
 export const MAX_USERNAME_LENGTH = 50;
 export const MIN_PASSWORD_LENGTH = 8;
 export const MAX_PASSWORD_LENGTH = 16;
+
+export const MAX_POST_TEXT_LENGTH = 1500;
+export const MAX_POST_CONTENT_LENGTH = 5;
