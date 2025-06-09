49bf6e86beb22971cceb1d488b64e98c155e0cac
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-06-09 11:44:50 +0545

    chore: capitalize

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { AUTH } from "./routes";
 import {
   LoginController,
-  registerControllerPost,
+  RegisterControllerPost,
 } from "../controller/auth/authController";
 
 const authRouter = Router();
