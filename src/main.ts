415f0b5824c5253ce7578776f700392cd1336ca2
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-06-14 20:52:16 +0545

    remove express list endpoints

--- src/main.ts
+++ src/main.ts
@@ -69,8 +69,5 @@ app.use(errorHandler());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  if (process.env.NODE_ENV !== "prod") {
-    console.log(require("express-list-endpoints")(app));
-  }
   console.log(`listening at port: http://localhost:${PORT}`);
 });
