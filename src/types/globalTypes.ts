5d5ceac49b1b350bccff3554f5d5d7492ec821d5
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-06-14 12:40:25 +0545

    add generic for table field selection

--- src/types/globalTypes.ts
+++ src/types/globalTypes.ts
@@ -1,1 +1,6 @@
+import { Selectable } from "kysely";
+
 export type ExpressUser = { id: string; username: string };
+export type TableFieldSelection<T> = Partial<
+  Record<keyof Selectable<T>, boolean>
+>;
