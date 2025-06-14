93126a2a85da8b45ec3a8c43a9bd58ea021a1e06
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-06-14 22:05:49 +0545

    remove dependency mark pino-pretty  in favour of devDependency

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -2,7 +2,7 @@ import pino, { destination, transport } from "pino";
 
 export const logger = pino(
   transport({
-    target: "pino-pretty",
+    target: process.env.NODE_ENV === "dev" ? "pino-pretty" : "pino/file",
     options: {
       destination: "./logs",
       colorize: false,
