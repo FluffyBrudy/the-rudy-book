abd9ca16f4652a62d6a03cca7c1e353fb0aa950c
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-06-20 23:44:03 +0545

    remove logger in prod

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,12 +1,17 @@
-import pino, { destination, transport } from "pino";
+import pino, { transport } from "pino";
 
-export const logger = pino(
-  transport({
-    target: process.env.NODE_ENV === "dev" ? "pino-pretty" : "pino/file",
-    options: {
-      destination: "./logs",
-      colorize: false,
-    },
-    level: "error",
-  })
-);
+let logger = { error(arg: any) {}, info(arg: any) {} };
+if (process.env.NODE_ENV === "dev") {
+  let logger = pino(
+    transport({
+      target: process.env.NODE_ENV === "dev" ? "pino-pretty" : "pino/file",
+      options: {
+        destination: "./logs",
+        colorize: false,
+      },
+      level: "error",
+    })
+  );
+}
+
+export { logger };
